73c2305993edf43419acc4cc178585f37c2e8a86
kadir <abduelkadirtetik@gmail.com>
2023-11-09 16:53:00 +0100

    levelfragment angepasst, Design überarbeitet

--- app/src/main/java/com/example/burger42/Fragments/LevelSelectionFragment.java
+++ app/src/main/java/com/example/burger42/Fragments/LevelSelectionFragment.java
@@ -1,23 +1,23 @@
-package com.example.burger42;
+package com.example.burger42.Fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import com.example.burger42.Fragments.ParentFragment;
+import com.example.burger42.MainActivity;
+import com.example.burger42.R;
 
-public class LevelSelectionActivity extends ParentFragment {
+public class LevelSelectionFragment extends ParentFragment {
 
     View view;
-
-    public LevelSelectionActivity(MainActivity mainActivity) {
+    public LevelSelectionFragment(MainActivity mainActivity) {
         super(mainActivity);
     }
 
@@ -26,6 +26,15 @@ public class LevelSelectionActivity extends ParentFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_level_selection, container, false);
         //createAndDisplayLevelItems();
+        Button backButton = (Button) view.findViewById(R.id.levelSelection_BackButton);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mainActivity.showFragment(new StartFragment(mainActivity), ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+            }
+        });
+
+
 
         return view;
     }
