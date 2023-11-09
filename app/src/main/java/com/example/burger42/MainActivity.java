1b0c33eec17d15652a0a7ba35f82adbc978a19c0
Till Rogoll <till.rogoll@study.hs-duesseldorf.de>
2023-11-09 14:05:55 +0100

    Settings Activity

--- app/src/main/java/com/example/burger42/MainActivity.java
+++ app/src/main/java/com/example/burger42/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.burger42;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.slider.Slider;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +24,17 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                startActivity(new Intent(MainActivity.this, LevelSelectionActivity.class));
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+                /*
+                View view = findViewById(R.id.background);
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                view.setBackgroundColor(color);
+
+                 */
             }
         });
 
     }
-
 
 }
