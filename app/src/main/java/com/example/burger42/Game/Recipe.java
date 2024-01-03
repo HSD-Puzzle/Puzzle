3513674ed852d14fc8ce0f37c9dcca8359651382
Jonas Kampshoff <Jonas.Kampshoff@study.hs-duesseldorf.de>
2024-01-03 13:29:53 +0100

    Order Page1

--- app/src/main/java/com/example/burger42/Game/Recipe.java
+++ app/src/main/java/com/example/burger42/Game/Recipe.java
@@ -1,6 +1,13 @@
 package com.example.burger42.Game;
 
+import com.example.burger42.Ingredients.Ingredient;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Recipe {
+
+    private List<List<Ingredient>> ingredients = new LinkedList<>();
 
     public enum PlaceToEat {
         ONSITE, TOGO
